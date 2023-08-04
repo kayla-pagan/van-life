ce58a0e1174890cd7692ff9c8f06ad61d23bcfdb
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-08-03 21:30:06 -0400

    fetch data and log to console in Vans file

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 export default function Vans(){
+    React.useEffect(() => {
+        async function fetchData(){
+            const response = await fetch('/api/vans')
+            const data = await response.json()
+            
+            console.log(data)
+        }
+        
+        fetchData()
+    }, [])
+
     return (
         <main>
             <h1>Explore our van options</h1>
