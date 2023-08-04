aeae95f85e75e34f2dfa205ba06960dce04d8947
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-08-03 22:20:32 -0400

    display data returned from fetch and style in Vans.jsx

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
+import React from "react"
 
-export default function Vans(){
+/**
+ * Challenge: Fetch and map over the data to display it on
+ * the vans page. For an extra challenge, spend time styling
+ * it to look like the Figma design.
+ * 
+ * Hints:
+ * 1. Use `fetch("/api/vans")` to kick off the request to get the
+ *    data from our fake Mirage JS server
+ * 2. What React hook would you use to fetch data as soon as the
+ *    Vans page loads, and only fetch it the one time?
+ */
+
+
+export default function Vans() {
+    const [vans, setVans] = React.useState([])
+    
+    console.log(vans)
+    
     React.useEffect(() => {
         async function fetchData(){
             const response = await fetch('/api/vans')
             const data = await response.json()
             
-            console.log(data)
+            setVans(data.vans)
         }
         
         fetchData()
     }, [])
-
+    
+    const vanElements = vans.map(van => (
+            <div key={van.id} className="van-tile">
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<br />/day</p>
+                </div>
+            </div>
+        ))
+    
     return (
-        <main>
-            <h1>Explore our van options</h1>
+        <main className="vans-container">
+        {vanElements}
         </main>
     )
 }
