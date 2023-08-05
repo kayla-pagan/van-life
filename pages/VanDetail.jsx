9ef09a3ac6764217e691217e1d05752b37ea9558
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-08-05 13:59:48 -0400

    create state to save van data in vanDetail pg

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -3,16 +3,17 @@ import { useParams } from "react-router-dom"
 
 export default function VanDetail(){
     const params = useParams()
+    const [van, setVan] = React.useState(null)
 
     React.useEffect(() => {
         async function getVan(){
             const response = await fetch(`/api/vans/${params.id}`)
             const data = await response.json()
-            console.log(data)
+            setVan(data.vans)
         }
 
         getVan()
-    }, [])
+    }, [params.id])
 
     return (
         <h1>Van detail page goes here</h1>
