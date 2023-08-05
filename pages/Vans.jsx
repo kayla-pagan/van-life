c3c381b09d53cc547bd24948125f96024cc7f8ed
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-08-05 13:01:22 -0400

    wrap each van in a Link tag

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 
 /**
  * Challenge: Fetch and map over the data to display it on
@@ -31,13 +32,15 @@ export default function Vans() {
     
     const vanElements = vans.map(van => (
             <div key={van.id} className="van-tile">
-                <img src={van.imageUrl} />
-                <div className="van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}<br />/day</p>
-                </div>
+                <Link to={`/vans/${van.id}`}>
+                    <img src={van.imageUrl} />
+                    <div className="van-info">
+                        <h3>{van.name}</h3>
+                        <p>${van.price}<br />/day</p>
+                    </div>
 
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                </Link>
             </div>
         ))
     
