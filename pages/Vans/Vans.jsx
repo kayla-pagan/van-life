2bee5e6ce4cee07edc70d4c03f696b56b259795c
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-08-05 21:28:19 -0400

    create Vans and Host folder under pages folder
    Move Vans.jsx and VanDetail.jsx under new vans folder and update paths in index.jsx

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,18 +1,6 @@
 import React from "react"
 import { Link } from 'react-router-dom'
 
-/**
- * Challenge: Fetch and map over the data to display it on
- * the vans page. For an extra challenge, spend time styling
- * it to look like the Figma design.
- * 
- * Hints:
- * 1. Use `fetch("/api/vans")` to kick off the request to get the
- *    data from our fake Mirage JS server
- * 2. What React hook would you use to fetch data as soon as the
- *    Vans page loads, and only fetch it the one time?
- */
-
 
 export default function Vans() {
     const [vans, setVans] = React.useState([])
