ab39bfa2613e612d183c356ef8e39c6310ff6d67
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-09-02 19:34:44 -0400

    add style to host dashboard linking to income

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,10 +1,21 @@
 import React from "react"
-
+import { Link } from "react-router-dom"
 
 export default function Dashboard(){
     return (
-        <main className="dashboard-main">
-            
+        <main className="dashboard--main">
+            <div className="dashboard--income">
+                <div>
+                    <h1>Welcome!</h1>
+                    <div className="income-info">
+                        <p>Income in the last <span>30 days</span></p>
+                        <Link to="/host/income">Details</Link>
+                    </div>
+                    <h2>$2,260</h2>
+                </div>
+            </div>
+            <div className="dashboard--reviews"></div>
+            <div className="dashboard--vans"></div>
         </main>
     )
 }
