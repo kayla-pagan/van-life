8e00f22cf601a2a95cc0d9e0086b87e903d3282f
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-09-02 19:11:32 -0400

    Add tailwind to dependencies

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -3,6 +3,8 @@ import React from "react"
 
 export default function Dashboard(){
     return (
-        <h1>Dashboard goes here</h1>
+        <main className="dashboard-main">
+            
+        </main>
     )
 }
