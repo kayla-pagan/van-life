d04c4dad325f148f98720feb09941ab28d7693c7
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-09-04 12:01:42 -0400

    install tailwind css from scratch again

--- index.jsx
+++ index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import Login from './pages/Login';
