eca22a8ca0348b8ac105dec480a6706f5fcad61d
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-09-07 21:53:09 -0400

    import bar chart items from chartJS

--- pages/Host/Income.jsx
+++ pages/Host/Income.jsx
@@ -1,7 +1,59 @@
 import React from "react"
+import { Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Tooltip
+} from "chart.js"
+import { Bar } from "react-chartjs-2"
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Tooltip
+)
+
+export const options = {
+    responsive: true,
+    scales: {
+        y: {
+            max: 5000,
+            min: 0,
+            ticks: { 
+                callback: function(value, index) {
+                    return index % 2 === 0 ? `$${value}` : '';
+                }
+        }
+    }
+}
+}
+
+const labels = ['Ju', 'Au', 'Se', 'Oc', 'No', 'De']
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            data: [4000, 1500, 3000, 2800, 1800, 500],
+            backgroundColor: [
+                'rgba(255, 234, 208, 1)',
+                'rgba(255, 234, 208, 1)',
+                'rgba(255, 234, 208, 1)',
+                'rgba(255, 234, 208, 1)',
+                'rgba(255, 140, 56, 1)',
+                'rgba(255, 140, 56, 1)'            
+            ]
+    }]
+}
 
 export default function Income(){
     return (
-        <h1>Income goes here</h1>
+        <main className="income--main">
+            <h1>Income</h1>
+            <p>Last <span>30 days</span></p>
+            <h2>$2,260</h2>
+            <Bar data={data} options={options} />
+        </main>
     )
 }
