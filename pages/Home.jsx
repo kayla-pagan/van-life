ac3f67d26381bdf7b179c5098f83665628f530d0
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2024-01-02 11:15:27 -0500

    add useNavigate hook to btn. lead to vans pg

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom' 
 
 export default function Home(){
+  const navigate = useNavigate()
+
     return (
       <main className="home--main">
         <div className="home--main-content">
             <h1>You got the travel plans, we got the travel vans.</h1>
             <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <button>Find your van</button>
+            <button onClick={() => navigate("/vans")}>Find your van</button>
         </div>
       </main>
     )
